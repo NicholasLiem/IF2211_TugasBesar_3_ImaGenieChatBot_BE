3168db2738ec6e9ae0d0c6d66a753b9f82b49da3
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-23 23:32:45 +0700

    style: naming and deleting stash

--- backend/cmd/routes.go
+++ backend/cmd/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/NicholasLiem/Tubes3_ImagineKelar/handlers/chat_session"
+	"github.com/NicholasLiem/Tubes3_ImagineKelar/handlers/messages"
 	"github.com/NicholasLiem/Tubes3_ImagineKelar/handlers/pages"
 	"github.com/NicholasLiem/Tubes3_ImagineKelar/handlers/question_answer"
 	"github.com/gofiber/fiber/v2"
@@ -9,11 +10,14 @@ import (
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/chat/", pages.ChatPage)
+
+	app.Post("/a/:session_id", messages.InsertMessageToChatSession)
+
 	app.Get("/h/:session_id", chat_session.GetChatMessages)
 	app.Post("/c/:session_id", chat_session.CreateChatSession)
 	app.Post("/r/:session_id", chat_session.DeleteChatSession)
+
 	app.Post("/create", question_answer.CreateQuestionAnswer)
 	app.Post("/remove", question_answer.DeleteQuestionAnswer)
-	app.Post("/a/:session_id", chat_session.InsertMessageToChatSession)
 	app.Get("/questions", question_answer.GetQuestionAnswers)
 }
