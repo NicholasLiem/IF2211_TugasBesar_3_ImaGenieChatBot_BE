8f689ee3225f7f30985e64224f737f5a0093ce0a
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-23 23:43:50 +0700

    docs: structuring files

--- backend/algorithms/date/date.go
+++ backend/algorithms/date/date.go
@@ -2,8 +2,8 @@ package date
 
 import (
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 const SUCCESS int = 0
@@ -12,35 +12,34 @@ const STRING_PARSING_ERR int = 2
 const ZERO_OR_LOWER_INPUT_ERR int = 3
 const INVALID_DATE_ERR int = 4
 
-var ErrorMessageList = []string {
+var ErrorMessageList = []string{
 	"",
 	"Not an integer",
 	"String parsing error",
-	"<= 0 input", 
+	"<= 0 input",
 	"Invalid date",
 }
 
-var DayList = []string {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"}
-var DaysInMonthsList = []int {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-
+var DayList = []string{"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"}
+var DaysInMonthsList = []int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 type Date struct {
-	input string;
-	d int;
-	m int;
-	y int;
-	valid bool;
-	dayResult string;
-	errorCode int;
-	errorMessage string;
+	input        string
+	d            int
+	m            int
+	y            int
+	valid        bool
+	dayResult    string
+	errorCode    int
+	errorMessage string
 }
 
-func StringToDate( s string) (bool, int, int, int, int) {
+func StringToDate(s string) (bool, int, int, int, int) {
 	arr := strings.Split(s, "/")
 	intArr := []int{}
 
 	if len(arr) == 3 {
-		for _, n := range arr{
+		for _, n := range arr {
 			val, err := strconv.Atoi(n)
 
 			if err != nil {
@@ -50,10 +49,10 @@ func StringToDate( s string) (bool, int, int, int, int) {
 		}
 
 		v, e := IsValidDate(intArr[0], intArr[1], intArr[2])
-		if (v){
+		if v {
 			return true, intArr[0], intArr[1], intArr[2], SUCCESS
 		} else {
-			return false, -999,-999,-999, e
+			return false, -999, -999, -999, e
 		}
 	} else {
 		return false, -999, -999, -999, STRING_PARSING_ERR
@@ -61,40 +60,40 @@ func StringToDate( s string) (bool, int, int, int, int) {
 
 }
 
-func IsValidDate( d int, m int, y int) (bool, int) {
+func IsValidDate(d int, m int, y int) (bool, int) {
 	if d < 1 || m < 1 || y < 1 {
 		return false, ZERO_OR_LOWER_INPUT_ERR
-	} else if d > 31 || m > 12{
+	} else if d > 31 || m > 12 {
 		return false, INVALID_DATE_ERR
 	} else {
-		if (m == 2){
-			if IsLeapYear(y){
-				if (d > 29){
+		if m == 2 {
+			if IsLeapYear(y) {
+				if d > 29 {
 					return false, INVALID_DATE_ERR
 				}
 			} else {
-				if ( d> DaysInMonthsList[2]){
+				if d > DaysInMonthsList[2] {
 					return false, INVALID_DATE_ERR
 				}
 			}
 			return true, SUCCESS
 		} else {
-			if (d > DaysInMonthsList[m]) {
+			if d > DaysInMonthsList[m] {
 				return false, INVALID_DATE_ERR
 			}
 			return true, SUCCESS
 		}
-		
+
 	}
 }
 
 func IsLeapYear(y int) bool {
-	if y % 4 != 0 {
+	if y%4 != 0 {
 		return false
 	} else {
-		if y % 400 == 0 {
+		if y%400 == 0 {
 			return true
-		} else if y % 100 == 0{
+		} else if y%100 == 0 {
 			return false
 		} else {
 			return true
@@ -112,19 +111,19 @@ func (date *Date) GetDayFromDate(s string) {
 	date.errorMessage = ErrorMessageList[date.errorCode]
 
 	// Progress further if Input is valid
-	if (date.valid){
+	if date.valid {
 		// Get amount of days so far from year
-		var days int = 365 * (date.y-1)
+		var days int = 365 * (date.y - 1)
 		days += AmountOfLeapYearSoFar(date.y)
 
-
 		// Get amount of days so far from month
-		for i := 1; i < date.m; i++{
+		for i := 1; i < date.m; i++ {
 			days += DaysInMonthsList[i]
 		}
 
-		if (IsLeapYear(date.y) && date.m > 2) { days++ }
-
+		if IsLeapYear(date.y) && date.m > 2 {
+			days++
+		}
 
 		// Get amount of days so far from date
 		days += date.d
@@ -132,14 +131,14 @@ func (date *Date) GetDayFromDate(s string) {
 		// Get the specific day
 		// Some references said adjustment is needed because 1 January 0001 is Saturday
 		// ADJUSTMENT := 5
-		var res int = (days ) % 7 
+		var res int = (days) % 7
 		date.dayResult = DayList[res]
 
 	}
 }
 
 func (date *Date) DisplayDate() {
-	if (date.valid){
+	if date.valid {
 		fmt.Println("========")
 		fmt.Println("Input: ", date.input)
 		fmt.Println("Date: ", date.d)
@@ -157,28 +156,28 @@ func (date *Date) DisplayDate() {
 		fmt.Println("Error Code: ", date.errorCode)
 		fmt.Println("Error Message: ", date.errorMessage)
 		fmt.Println("========")
-		
+
 	}
 }
 
 func (date *Date) ResetDate() {
 	date.input = ""
-	date.d = 0;
-	date.m = 0;
-	date.y = 0;
-	date.dayResult = "";
-	date.valid = true;
-	date.errorCode = 0;
-	date.errorMessage = "";
+	date.d = 0
+	date.m = 0
+	date.y = 0
+	date.dayResult = ""
+	date.valid = true
+	date.errorCode = 0
+	date.errorMessage = ""
 }
 
 func AmountOfLeapYearSoFar(y int) int {
 	count := 0
 	for i := 1; i < y; i++ {
-		if (IsLeapYear(i)){
+		if IsLeapYear(i) {
 			count++
 		}
-	}		
+	}
 	return count
 }
 
